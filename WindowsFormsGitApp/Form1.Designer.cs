ef71a254a8414058a52696230d566cc323633cb4
PauliNiklas <PauliNiklas@dv-schulen.de>
2017-10-24 09:04:04 +0200

    ll

--- WindowsFormsGitApp/Form1.Designer.cs
+++ WindowsFormsGitApp/Form1.Designer.cs
@@ -35,9 +35,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(197, 116);
+            this.button1.Location = new System.Drawing.Point(49, 54);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(183, 132);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
